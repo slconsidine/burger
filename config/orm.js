7e4d6d149fb4223541d18ff6b4940793c8011dbd
Sam <samlconsidine@gmail.com>
2020-03-29 12:34:19 -0400

    Added all initial files and all folders.

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function() {
+        // function code goes here
+    },
+    insertOne: function() {
+        // function code goes here
+    },
+    updateOne: function() {
+        // function code goes here
+    }
+  };
+  
+
+  module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,15 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+}
+
+module.exports = burger;
