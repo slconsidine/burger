eb98a8df3a15993b846267bac3a907ca7db4ab8e
Sam <samlconsidine@gmail.com>
2020-03-30 09:03:07 -0400

    Database connection being initiated properly.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "mysql",
-  database: "burgers_db"
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DATABASE
 });
+
 
 connection.connect(function(err) {
   if (err) {
