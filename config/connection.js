79b42f6dfe71b3b70f3bd85b33f00d94d4d4c365
Sam <samlconsidine@gmail.com>
2020-03-30 10:01:43 -0400

    Adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.DATABASE
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE
+  })
+}
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: process.env.USER,
+//   password: process.env.PASS,
+//   database: process.env.DATABASE
+// });
 
 connection.connect(function(err) {
   if (err) {
